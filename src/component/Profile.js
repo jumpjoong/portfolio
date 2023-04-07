a6c2640d8f82d8a6f447f622802c74d466cf51b6
jumpgoong <khj0196@gmail.com>
2023-04-07 16:14:07 +0900

    stciky add

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import '../styles/profile.scss'
 
 function Profile() {
   return (
-    <>
-    <div>Profile</div>
-    </>
+    <div className="profile">
+      <div className="profile-wrap">
+        <div className="first">
+          <figure>
+            <div>
+              <img src="./img/music.jpg" alt="무식은나의삶이다"/>
+            </div>
+            <figcaption>
+              <p>설명글 영역</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="second">
+          <div className="time-line">
+            <p>타임라인</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
